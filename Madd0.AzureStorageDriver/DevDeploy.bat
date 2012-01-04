a323b73671213694beafc51f653df630aa879916
Mauricio DIAZ ORLICH <github@madd0.com>
2012-01-04 23:17:45 +0100

    Corrected FX version in post-build bat.

--- Madd0.AzureStorageDriver/DevDeploy.bat
+++ Madd0.AzureStorageDriver/DevDeploy.bat
@@ -5,5 +5,5 @@ rem  It copies the output .DLL (and .PDB) to LINQPad's drivers folder, so that L
 rem  picks up the drivers immediately (without needing to click 'Add Driver').
 rem
 
-xcopy /i/y Madd0.AzureStorageDriver.dll "%AllUsersProfile%\LINQPad\Drivers\DataContext\3.5\Madd0.AzureStorageDriver (47842961fb3025d7)\"
-xcopy /i/y Madd0.AzureStorageDriver.pdb "%AllUsersProfile%\LINQPad\Drivers\DataContext\3.5\Madd0.AzureStorageDriver (47842961fb3025d7)\"
+xcopy /i/y Madd0.AzureStorageDriver.dll "%AllUsersProfile%\LINQPad\Drivers\DataContext\4.0\Madd0.AzureStorageDriver (47842961fb3025d7)\"
+xcopy /i/y Madd0.AzureStorageDriver.pdb "%AllUsersProfile%\LINQPad\Drivers\DataContext\4.0\Madd0.AzureStorageDriver (47842961fb3025d7)\"
